3022f638774456d6258322fa2e8d675fbc503337
Gianfranco Damato <gianfranco.damato@hotmail.it>
2021-01-31 14:10:14 +0100

    We have a random meeting point

--- Assets/GGJ/Scripts/MeetingAreaSpawner.cs
+++ Assets/GGJ/Scripts/MeetingAreaSpawner.cs
@@ -7,11 +7,13 @@ public class MeetingAreaSpawner : MonoBehaviour
 {
     public GameObject TriggerAreaPrefab;
 
+    public Transform[] Hexes;
+
     private void Start()
     {
         if(PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate(TriggerAreaPrefab.name, Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate(TriggerAreaPrefab.name, Hexes[Random.Range(0, Hexes.Length)].position, Quaternion.identity);
         }
     }
 }
